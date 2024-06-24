9778dc5a17dfbcd81d34e967a1b5d98367759acd
Alejandro Montoya <isc.alejandro14@gmail.com>
2024-06-23 20:03:52 -0600

    Se sube archivo json

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public record Pelicula() {
+//son los atributos del json
+public record Pelicula(String title,
+                       int episode_id,
+                       String opening_crawl,
+                       String director,
+                       String producer,
+                       String release_date) {
+
 }
+
